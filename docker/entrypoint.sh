40e534be2c6d83f5b8cc3e24d4fab49bbae442ed
rohitkarki483 <141471593+rohitkarki483@users.noreply.github.com>
2026-01-17 23:50:22 +0545

    Create entrypoint.sh

---
+++ docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Fixing Laravel permissions..."
+
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache 2>/dev/null || true
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache 2>/dev/null || true
+
+exec php-fpm
